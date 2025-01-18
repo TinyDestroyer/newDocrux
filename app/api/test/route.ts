95fa5d21bfd853f915d9bdd348edd3be2abbd25b
TinyDestroyer <guptashubh.20208@gmail.com>
2025-01-18 13:14:43 +0530

    fourtyoneth commit

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,18 +1,30 @@
 import { NextResponse } from "next/server"
+import { Pinecone } from '@pinecone-database/pinecone';
+import { HfInference } from '@huggingface/inference';
 import Groq from "groq-sdk";
+
+const hf = new HfInference(process.env.HF_TOKEN);
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 export async function GET(req : Request){
     const { searchParams } = new URL(req.url);
     console.log("shubh");
-    const query = searchParams.get("query");
-    const prompt = "" + query;
+    const query = searchParams.get("query") || "";
+
+    const embed_query = await hf.featureExtraction({
+        model: 'sentence-transformers/all-MiniLM-L6-v2',
+        inputs: query,
+    }).catch((error) => {
+        console.error("Error during Hugging Face feature extraction:", error);
+        throw error;
+    });
+
     const data = await groq.chat.completions.create({
             messages: [
               {
                 role: "user",
-                content: prompt,
+                content: query,
               },
             ],
             model: "llama-3.3-70b-versatile",

17cb08d29444df15e249b4f6fa1c12a7da6e9746
TinyDestroyer <guptashubh.20208@gmail.com>
2025-01-19 02:17:34 +0530

    Done

--- app/(site)/_components/Logo.tsx
+++ app/(site)/_components/Logo.tsx
@@ -6,7 +6,7 @@ const Logo = (props: Props) => {
   return (
     <a href="/" className="flex items-center gap-2">
       <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
-        ChatPDF
+        Docrux
       </p>
     </a>
   );

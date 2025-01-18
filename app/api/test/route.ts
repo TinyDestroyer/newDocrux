d7e82172b1eac7c29b84e5284ea3912989a61619
TinyDestroyer <guptashubh.20208@gmail.com>
2025-01-18 12:13:33 +0530

    thirtynineth commit

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server"
 
 export async function GET(req : Request){
     const { searchParams } = new URL(req.url);
-    console.log(searchParams);
+    console.log("shubh");
     const user = searchParams.get("query");
     return NextResponse.json(user);
 }

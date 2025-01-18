16682caad1f7af75142fe8e107844620aa80ca6b
TinyDestroyer <guptashubh.20208@gmail.com>
2025-01-18 13:09:29 +0530

    fourtyth commit

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,8 +1,22 @@
 import { NextResponse } from "next/server"
+import Groq from "groq-sdk";
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 export async function GET(req : Request){
     const { searchParams } = new URL(req.url);
     console.log("shubh");
-    const user = searchParams.get("query");
-    return NextResponse.json(user);
+    const query = searchParams.get("query");
+    const prompt = "" + query;
+    const data = await groq.chat.completions.create({
+            messages: [
+              {
+                role: "user",
+                content: prompt,
+              },
+            ],
+            model: "llama-3.3-70b-versatile",
+            max_tokens: 300,
+        });
+    return NextResponse.json(data.choices[0].message.content);
 }

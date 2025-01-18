268f648653742802f785180a2ab3e9e32246e24e
TinyDestroyer <guptashubh.20208@gmail.com>
2025-01-18 13:20:13 +0530

    fourtytwoth commit

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -7,10 +7,16 @@ const hf = new HfInference(process.env.HF_TOKEN);
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
+const pinecone = new Pinecone({
+  apiKey: process.env.PINECONE_API_KEY!
+});
+const index = pinecone.index('docrux');
+
 export async function GET(req : Request){
     const { searchParams } = new URL(req.url);
     console.log("shubh");
     const query = searchParams.get("query") || "";
+    const user = searchParams.get("user");
 
     const embed_query = await hf.featureExtraction({
         model: 'sentence-transformers/all-MiniLM-L6-v2',
@@ -20,11 +26,36 @@ export async function GET(req : Request){
         throw error;
     });
 
+    let queryEmbedding: number[];
+
+    if (Array.isArray(embed_query) && Array.isArray(embed_query[0])) {
+            // If it's a nested array, take the first array
+            queryEmbedding = embed_query[0] as number[];
+        } else if (Array.isArray(embed_query)) {
+            // If it's already a flat array
+            queryEmbedding = embed_query as number[];
+        } else {
+            // If it's a single number (shouldn't happen with this model)
+            queryEmbedding = [embed_query as number];
+        }
+
+        const response = await index.query({
+            topK: 2,
+            vector: queryEmbedding,
+            includeValues: true,
+            includeMetadata: true,
+            filter: { user }
+        });
+        let text = "";
+        for(let i = 0; i < response.matches.length; i++){
+            text += response.matches[i]?.metadata?.text;
+        }
+        const prompt = text + "," + query;
     const data = await groq.chat.completions.create({
             messages: [
               {
                 role: "user",
-                content: query,
+                content: prompt,
               },
             ],
             model: "llama-3.3-70b-versatile",
